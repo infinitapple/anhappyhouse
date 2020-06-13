578044a234d25f1add7d0712796cdae74288fc15
dlgnfkr <dlgnfkr234@naver.com>
2020-06-13 17:02:19 +0900

    build update

--- docs/js/chunk-2d0de905.31c9a903.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0de905"],{"85d6":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" update page ")])},a=[],c={name:"userupdate"},d=c,l=t("2877"),p=Object(l["a"])(d,u,a,!1,null,null,null);n["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d0de905.31c9a903.js.map
--- docs/js/chunk-2d20e823.1bad68e1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20e823"],{b054:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" info page ")])},a=[],c={name:"userinfo"},i=c,l=t("2877"),o=Object(l["a"])(i,u,a,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d20e823.1bad68e1.js.map
